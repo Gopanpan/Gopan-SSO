d78063dee5fac111fc56d855552c115fbf2d2386
panlinsen <Pan@5201314>
2017-01-25 11:38:46 +0800

    添加一些API配置

---
+++ sso-api/src/main/java/bing/Pan/sso/api/SsoAIPApplication.java
@@ -0,0 +1,49 @@
+package bing.Pan.sso.api;
+
+
+import bing.Pan.sso.service.config.dynamicDataSource.DynamicDataSourceRegister;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * @crea :Created by intelliJ IDEA 16.1.1 .
+ * @auth :bing.Pan 15923508369@163.com .
+ * @date :2017/1/16 20:02
+ * @desc :
+ */
+@SpringBootApplication
+@ComponentScan(value = "bing.Pan")
+@Import({DynamicDataSourceRegister.class})
+public class SsoAIPApplication {
+
+
+    /**
+     * 配置Spring Boot完美使用FastJson解析JSON数据
+     * fastjson 最低要求就是1.2.10+
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SsoAIPApplication.class, args);
+
+    }
+}
