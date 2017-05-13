69bdca992858d3d03ff361837ff8eb871ae8233f
bing.Pan <15923508369@163.com>
2017-05-13 22:23:12 +0800

    页面错误异常样式修改

--- sso-manage/src/main/resources/static/js/custom/mylib.js
+++ sso-manage/src/main/resources/static/js/custom/mylib.js
@@ -82,7 +82,7 @@ function availableFormat(avaliable){
  */
 function serviceErrorValidate(data){
     if(data.code != 20000 || data.code != '20000'){
-        layer.msg("服务器异常<br/>异常代码："+data.code+",异常内容："+data.message,{icon:5,time:3000});
+        layer.msg("服务器异常<br/>异常代码："+data.code+"<br/>异常内容："+data.message,{icon:5,time:3000});
     }
 }
 
