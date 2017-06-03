95689bc5da037872d18dd9ce40e50acba55bff79
bing.Pan <15923508369@163.com>
2017-06-03 15:45:40 +0800

    增加bean 与 map的互转

---
+++ sso-manage/src/test/java/bing/Pan/sso/Test.java
@@ -0,0 +1,40 @@
+package bing.Pan.sso;
+
+import bing.Pan.sso.common.utils.BeanMapUtil;
+import bing.Pan.sso.domain.entity.SysUser;
+import com.google.common.collect.Maps;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @crea : Created by intelliJ IDEA 16.1.3
+ * @auth : bing.Pan
+ * @mail : 15923508369@163.com
+ * @date : 2017/6/3 14:11
+ * @desc :
+ */
+public class Test {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IntrospectionException, InstantiationException {
+        SysUser sysUser = new SysUser();
+        sysUser.setId(1L);
+        sysUser.setEmail("15923508369@163.com");
+        sysUser.setCreateTime(new Date());
+
+        Map<String, Object> stringObjectMap = BeanMapUtil.beanTrans2Map(sysUser);
+
+
+        System.out.println(stringObjectMap.toString());
+
+
+        SysUser user = new SysUser();
+        Object o = BeanMapUtil.convertMap(user.getClass(), stringObjectMap);
+        System.out.println(o.toString());
+
+    }
+}
--- sso-mapper/src/main/java/bing/Pan/sso/mapper/mapperInterface/SsoOrganizationMapper.java
+++ sso-mapper/src/main/java/bing/Pan/sso/mapper/mapperInterface/SsoOrganizationMapper.java
@@ -1,6 +1,7 @@
 package bing.Pan.sso.mapper.mapperInterface;
 
+import bing.Pan.sso.domain.entity.SsoOrganization;
 import bing.Pan.sso.mapper.BaseMapper;
 
-public interface SsoOrganizationMapper extends BaseMapper<SsoOrganizationMapper,Object>{
+public interface SsoOrganizationMapper extends BaseMapper<SsoOrganization,Object>{
 }
