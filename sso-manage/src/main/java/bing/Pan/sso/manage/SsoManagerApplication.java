028a8bc922b9227d4972e4d87648bd88ed191f31
bing.Pan <15923508369@163.com>
2017-06-05 14:31:27 +0800

    添加 session listener 监听器

--- sso-manage/src/main/java/bing/Pan/sso/manage/SsoManagerApplication.java
+++ sso-manage/src/main/java/bing/Pan/sso/manage/SsoManagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -21,6 +22,7 @@ import java.util.concurrent.TimeUnit;
  */
 @SpringBootApplication
 @ComponentScan(value = "bing.Pan")
+@ServletComponentScan
 @Import({DynamicDataSourceRegister.class})
 public class SsoManagerApplication {
 
---
+++ sso-manage/src/main/java/bing/Pan/sso/manage/common/listener/SessionListener.java
@@ -0,0 +1,35 @@
+package bing.Pan.sso.manage.common.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.text.MessageFormat;
+
+/**
+ * @crea : Created by intelliJ IDEA 16.1.3
+ * @auth : bing.Pan
+ * @mail : 15923508369@163.com
+ * @date : 2017/6/5 13:39
+ * @desc :
+ */
+
+@WebListener
+public class SessionListener  implements HttpSessionListener{
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+
+        String id = httpSessionEvent.getSession().getId();
+        System.out.println(MessageFormat.format("sessionCreated,sessionid为：{0}",id));
+
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+        String id = httpSessionEvent.getSession().getId();
+        System.out.println(MessageFormat.format("sessionDestroyed,sessionid为：{0}",id));
+
+    }
+}
