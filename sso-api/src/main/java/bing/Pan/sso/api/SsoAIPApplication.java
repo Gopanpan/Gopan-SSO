5c0a68204e7a675d81ba9d9060c2ef23332a685d
panlinsen <panlinsen@ucfgroup.com>
2017-11-13 16:54:12 +0800

    SsoApiApplication 集成fastjson 后对 中文乱码问题处理

--- sso-api/src/main/java/bing/Pan/sso/api/SsoAIPApplication.java
+++ sso-api/src/main/java/bing/Pan/sso/api/SsoAIPApplication.java
@@ -11,7 +11,11 @@ import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @crea :Created by intelliJ IDEA 16.1.1 .
@@ -36,6 +40,12 @@ public class SsoAIPApplication {
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
         fastConverter.setFastJsonConfig(fastJsonConfig);
+
+        //处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<MediaType>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+
         HttpMessageConverter<?> converter = fastConverter;
         return new HttpMessageConverters(converter);
     }
